751e9579a733c6ed828d7b639a4716ec2a61e393
AntonVoronezh <pstut@mail.ru>
2019-06-12 20:29:57 +0300

    ref ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-
+import swapiService from '../../services/swapiService';
+import Spinner from '../Spinner';
 import './ItemList.css';
 
 export default class ItemList extends Component {
+	swapi = new swapiService();
 
-  render() {
-    return (
-      <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
-      </ul>
-    );
-  }
+	state = {
+		peopleList: null,
+	};
+
+	componentDidMount() {
+		this.swapi.getAllPeople().then(peopleList => {
+			this.setState({ peopleList });
+		});
+	}
+
+	renderItems(arr) {
+		return arr.map(({ id, name }) => {
+			return (
+				<li key={id} className="list-group-item" onClick={() => this.props.onItemSelected(id)}>
+					{name}
+				</li>
+			);
+		});
+	}
+
+	render() {
+		const { peopleList } = this.state;
+		if (!peopleList) {
+			return <Spinner />;
+		}
+
+		const items = this.renderItems(peopleList);
+
+		return <ul className="item-list list-group">{items}</ul>;
+	}
 }
