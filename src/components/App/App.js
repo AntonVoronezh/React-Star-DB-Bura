e3b1b256a6a75d0074b7811efd15674968fe46ed
AntonVoronezh <pstut@mail.ru>
2019-06-12 20:29:34 +0300

    reg Add

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../Header';
 import RandomPlanet from '../RandomPlanet';
@@ -7,22 +7,45 @@ import PersonDetails from '../PersonDetails';
 
 import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
+	state = {
+		showRandomPlanet: true,
+		selectedPerson: 5,
+	};
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
-        </div>
-      </div>
-    </div>
-  );
-};
+	toggleRandomPlanet = () => {
+		this.setState(state => {
+			return {
+				showRandomPlanet: !state.showRandomPlanet,
+			};
+		});
+	};
 
-export default App;
+	onPersonSelected = id => {
+		this.setState({ selectedPerson: id });
+	};
+
+	render() {
+		const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+
+		return (
+			<div>
+				<Header />
+				{planet}
+
+				<button className="toggle-planet btn btn-warning btn-lg" onClick={this.toggleRandomPlanet}>
+					Toggle Random Planet
+				</button>
+
+				<div className="row mb2">
+					<div className="col-md-6">
+						<ItemList onItemSelected={this.onPersonSelected} />
+					</div>
+					<div className="col-md-6">
+						<PersonDetails personId={this.state.selectedPerson} />
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
