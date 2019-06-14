377c72e60a13cd77fa218b916b818065041b70b0
AntonVoronezh <pstut@mail.ru>
2019-06-14 17:06:14 +0300

    ref Record

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -9,19 +9,18 @@ export default class ItemDetails extends React.Component {
 	componentDidMount() {
 		const { getImg, id } = this.props;
 
-		getImg(id).then(({url}) => {
-			this.setState({ imgUrl:url });
+		getImg(id).then(({ url }) => {
+			this.setState({ imgUrl: url });
 		});
 	}
 
 	componentDidUpdate() {
 		const { getImg, id } = this.props;
 
-		getImg(id).then(({url}) => {
-			this.setState({ imgUrl:url });
+		getImg(id).then(({ url }) => {
+			this.setState({ imgUrl: url });
 		});
 	}
-
 
 	render() {
 		const { name, gender, birthYear, eyeColor } = this.props;
@@ -41,10 +40,6 @@ export default class ItemDetails extends React.Component {
 							<span className="term">Birth Year</span>
 							<span>{birthYear}</span>
 						</li>
-						<li className="list-group-item">
-							<span className="term">Eye Color</span>
-							<span>{eyeColor}</span>
-						</li>
 					</ul>
 				</div>
 			</div>
@@ -52,6 +47,11 @@ export default class ItemDetails extends React.Component {
 	}
 }
 
-export const Record = ({item, field, label}) => {
-
-}
+export const Record = ({ item, field, label }) => {
+	return (
+		<li className="list-group-item">
+			<span className="term">{label}</span>
+			<span>{item[field]}</span>
+		</li>
+	);
+};
