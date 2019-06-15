2121a1b99f8ff1449f8287f76998b6f13e24d91e
AntonVoronezh <pstut@mail.ru>
2019-06-15 16:32:27 +0300

    ref Pages - add SwapiConsumer

--- src/components/Pages/Pages.js
+++ src/components/Pages/Pages.js
@@ -1,70 +1,70 @@
 import React from 'react';
 import Page from '../Page';
-import swapiService from '../../services/swapiService';
+import { SwapiConsumer } from '../../services/swapiContext';
 import { Record } from '../ItemDetails/ItemDetails';
-
-const {
-	getAllPeople,
-	getAllPlanets,
-	getAllStarships,
-	getPersonImage,
-	getPlanetImage,
-	getStarshipImage,
-	getPersonById,
-	getStarshipById,
-	getPlanetById,
-} = new swapiService();
 
 const PeoplePage = () => {
 	return (
-		<Page
-			getData={getAllPeople}
-			getImg={getPersonImage}
-			getById={getPersonById}
-			renderItem={({ name, birthYear }) => `${name} (${birthYear})`}
-		>
-			<Record field="birthYear" label="Birth Year" />
-			<Record field="eyeColor" label="Eye Color" />
-			<Record field="gender" label="Gender" />
-		</Page>
+		<SwapiConsumer>
+			{({ getAllPeople, getPersonImage, getPersonById }) => (
+				<Page
+					getData={getAllPeople}
+					getImg={getPersonImage}
+					getById={getPersonById}
+					renderItem={({ name, birthYear }) => `${name} (${birthYear})`}
+				>
+					<Record field="birthYear" label="Birth Year" />
+					<Record field="eyeColor" label="Eye Color" />
+					<Record field="gender" label="Gender" />
+				</Page>
+			)}
+		</SwapiConsumer>
 	);
 };
 
 const PlanetsPage = () => {
 	return (
-		<Page
-			getData={getAllPlanets}
-			getImg={getPlanetImage}
-			getById={getPlanetById}
-			renderItem={({ name, population }) => `${name} (${population})`}
-		>
-			<Record field="diameter" label="Diameter" />
-			<Record field="population" label="Population" />
-			<Record field="rotationPeriod" label="Rotation Period" />
-		</Page>
+		<SwapiConsumer>
+			{({ getAllPlanets, getPlanetImage, getPlanetById }) => (
+				<Page
+					getData={getAllPlanets}
+					getImg={getPlanetImage}
+					getById={getPlanetById}
+					renderItem={({ name, population }) => `${name} (${population})`}
+				>
+					<Record field="diameter" label="Diameter" />
+					<Record field="population" label="Population" />
+					<Record field="rotationPeriod" label="Rotation Period" />
+				</Page>
+			)}
+		</SwapiConsumer>
 	);
 };
 const StarshipsPage = () => {
 	return (
-		<Page
-			getData={getAllStarships}
-			getImg={getStarshipImage}
-			getById={getStarshipById}
-			renderItem={({ name, model }) => (
-				<>
-					<span>{name}</span>
-					<button>{model}</button>
-				</>
+		<SwapiConsumer>
+			{({ getAllStarships, getStarshipImage, getStarshipById }) => (
+				<Page
+					getData={getAllStarships}
+					getImg={getStarshipImage}
+					getById={getStarshipById}
+					renderItem={({ name, model }) => (
+						<>
+							<span>{name}</span>
+							<button>{model}</button>
+						</>
+					)}
+				>
+					<Record field="model" label="Model" />
+					<Record field="manufacturer" label="Manufacturer" />
+					<Record field="length" label="Length" />
+					<Record field="costInCredits" label="Cost" />
+					<Record field="crew" label="Crew" />
+					<Record field="passengers" label="Passengers" />
+					<Record field="cargoCapacity" label="Cargo Capacity" />
+				</Page>
 			)}
-		>
-			<Record field="model" label="Model" />
-			<Record field="manufacturer" label="Manufacturer" />
-			<Record field="length" label="Length" />
-			<Record field="costInCredits" label="Cost" />
-			<Record field="crew" label="Crew" />
-			<Record field="passengers" label="Passengers" />
-			<Record field="cargoCapacity" label="Cargo Capacity" />
-		</Page>
+		</SwapiConsumer>
 	);
 };
 
