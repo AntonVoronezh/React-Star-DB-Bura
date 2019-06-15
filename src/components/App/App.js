becb88bd832d1721b3e2156665745cb38d525b8e
AntonVoronezh <pstut@mail.ru>
2019-06-15 16:14:49 +0300

    ref App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import Header from '../Header';
 import RandomPlanet from '../RandomPlanet';
-import Page from '../Page';
 import ErrorButton from '../ErrorButton';
 import ErrorIndicator from '../ErrorIndicator';
-import swapiService from '../../services/swapiService';
-import { Record } from '../ItemDetails/ItemDetails';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../Pages';
 import './App.css';
 
 export default class App extends Component {
-	swapi = new swapiService();
 
 	state = {
 		showRandomPlanet: true,
@@ -36,18 +32,6 @@ export default class App extends Component {
 
 		const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 
-		const {
-			getAllPeople,
-			getAllPlanets,
-			getAllStarships,
-			getPersonImage,
-			getPlanetImage,
-			getStarshipImage,
-			getPersonById,
-			getStarshipById,
-			getPlanetById,
-		} = this.swapi;
-
 		return (
 			<div>
 				<Header />
@@ -57,56 +41,11 @@ export default class App extends Component {
 				</button>
 				<ErrorButton />
 
-				
-			
-
-
 				<PeoplePage />
-				{/* <Page
-					getData={getAllPeople}
-					getImg={getPersonImage}
-					getById={getPersonById}
-					renderItem={({ name, birthYear }) => `${name} (${birthYear})`}
-				>
-					<Record field="birthYear" label="Birth Year" />
-					<Record field="eyeColor" label="Eye Color" />
-					<Record field="gender" label="Gender" />
-				</Page> */}
 
-<PlanetsPage />
+				<PlanetsPage />
 
-				{/* <Page
-					getData={getAllPlanets}
-					getImg={getPlanetImage}
-					getById={getPlanetById}
-					renderItem={({ name, population }) => `${name} (${population})`}
-				>
-					<Record field="diameter" label="Diameter" />
-					<Record field="population" label="Population" />
-					<Record field="rotationPeriod" label="Rotation Period" />
-				</Page> */}
-
-<StarshipsPage />
-
-				{/* <Page
-					getData={getAllStarships}
-					getImg={getStarshipImage}
-					getById={getStarshipById}
-					renderItem={({ name, model }) => (
-						<>
-							<span>{name}</span>
-							<button>{model}</button>
-						</>
-					)}
-				>
-					<Record field="model" label="Model" />
-					<Record field="manufacturer" label="Manufacturer" />
-					<Record field="length" label="Length" />
-					<Record field="costInCredits" label="Cost" />
-					<Record field="crew" label="Crew" />
-					<Record field="passengers" label="Passengers" />
-					<Record field="cargoCapacity" label="Cargo Capacity" />
-				</Page> */}
+				<StarshipsPage />
 			</div>
 		);
 	}
