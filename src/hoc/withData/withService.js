b74e92767129c652cb000ff9c186c225002a9aa2
AntonVoronezh <pstut@mail.ru>
2019-06-16 15:03:09 +0300

    ref withService

--- src/hoc/withData/withService.js
+++ src/hoc/withData/withService.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { SwapiConsumer } from '../../services/swapiContext';
 
-const withService = Wrapped => {
+const withService = (Wrapped, mapMethodsToProps) => {
 	return props => (
 		<SwapiConsumer>
-			{
-				SwapiService => (
-					<Wrapped {...props} SwapiService={SwapiService}/>
-				)
-			}
+			{SwapiService => {
+				const propsService = mapMethodsToProps(SwapiService);
+
+				return <Wrapped {...props} {...propsService} />;
+			}}
 		</SwapiConsumer>
-		
-	)
+	);
 };
 
 export default withService;
